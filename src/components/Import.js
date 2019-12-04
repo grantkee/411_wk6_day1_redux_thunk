bd4dc79f74e884969933ca63b0a325837164bb36
grantkee <grant.d.kee@gmail.com>
2019-12-04 14:21:20 -0600

    import button is creating list. working on adding delete button and verticle drop-down menu button

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import {
+    Container,
+    Table,
+    Button,
+    TableHead,
+    TableBody,
+    TableRow,
+    TableCell
+} from '@material-ui/core'
 
 const Import = (props) => {
     // fill out this component
 
     return (
-        <p>Import Component</p>
+        <>
+        <Button variant='contained' color='primary' onClick={props.fetchMakes}>Import</Button>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Make</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {props.makes.map( car => (
+                    <TableRow key={car.MakeId}>
+                        <TableCell>{car['MakeId']}</TableCell>
+                        <TableCell>{car['MakeName']}</TableCell>
+                        <TableCell>Action</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+        </>
     )
 }
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,5 @@
+import { generateKeyPairSync } from "crypto"
+
 const url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
 
 export const addCar = (car) => {
@@ -11,5 +13,20 @@ export const removeCar = (index) => {
     return {
         type: 'REMOVE_CAR',
         value: index
+    }
+}
+
+export const fetchMakes = () => {
+    return( dispatch ) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(response => {
+            const action = {
+                type: 'FETCH_MAKES',
+                value: response.Results
+            }
+            dispatch(action)
+        })
+        .catch(error => console.log(error))
     }
 }
